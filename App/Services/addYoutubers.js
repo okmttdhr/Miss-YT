5039d5d72a3ebd787e9b43b9eb9fbf4dee8c8496
okmttdhr <10101114fin@gmail.com>
2017-02-03 20:50:43 +0800

    initialize firebase

--- App/Services/addYoutubers.js
+++ App/Services/addYoutubers.js
@@ -1,7 +1,20 @@
 // @flow
 
+import firebase from 'firebase'
+import Secrets from 'react-native-config'
+
+const firebaseConfig = {
+  apiKey: Secrets.API_KEY,
+  authDomain: Secrets.AUTH_DOMAIN,
+  databaseURL: Secrets.DATABASE_URL,
+  storageBucket: Secrets.STORAGE_BUCKET,
+  messagingSenderId: Secrets.MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(firebaseConfig)
 const log = () => {
   console.log('batch finished')
+  console.log(firebase)
 }
 
 export const addYoutubers = log
