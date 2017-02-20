f1ae323c17b8744046b9cb069aa19e362d9b461f
okmttdhr <10101114fin@gmail.com>
2017-02-20 22:01:16 +0800

    update score and rank

--- App/Services/batches/updateChannels.js
+++ App/Services/batches/updateChannels.js
@@ -1,20 +1,16 @@
 // @flow
-// -------------大まかな流れ----------------
-// 1.チャンネル登録者数とフォロワーを取得、更新
-// 2.更新した数値から`score`を作成
-// 3.`score`をorderに指定してデータを取得、indexをrankとして保存
+import Promise from 'bluebird'
 import {toString} from 'lodash'
-import {ChannelsResource, channelsRef} from '../index'
+import {ChannelsResource, channelsRef, logFinished} from '../index'
+import type {TChannel} from '../firebaseRef'
 
+// the parameter limit of TwitterUsersLookupResource's `screen_name`
 const LIMIT = 100
+type ChannelsSnapshot = {[key: string]: TChannel}
 
-// 100個の`CHANNEL_IDS``SCREEN_NAME`を配列に保持
 const accumulateIds = (snapshot) => {
-  const channelIds = []
-  snapshot.forEach((channel) => {
-    const c = channel.val()
-    channelIds.push(c.youtube.id)
-  })
+  const channels: ChannelsSnapshot = snapshot.val()
+  const channelIds = Object.keys(channels).map((key, index) => channels[key].youtube.id)
   return {channelIds}
 }
 
@@ -24,34 +20,62 @@ const toParameter = (ids) => {
   }
 }
 
-// まとめてリクエストパラメーターとして投げる
-// =>リクエストは1回で済む
-// =>返ってくる順番はバラバラ
 const getLatestItem = (channelIds, screenNames) => {
+  console.log('getLatestItem')
   const channelsResource = new ChannelsResource()
-  // Twitterのレスポンスも含めた`{channels: item, twitter, item}`というようなオブジェクトに変更できる設計で返している
+  // change to something like `{channels: item, twitter, item}`, when you add TwitterUsersLookupResource
   return channelsResource.get(channelIds).then((res) => ({channels: res.data.items}))
 }
 
-// 100件のレスポンスを回しながら、該当するFirebaseのデータをアップデートしてゆく
-// =>100件の検索と更新くらいならFirebaseはやられない。
-// =>もしかしたら時間はかかるかもしれないが、それはbatchなので問題なし。
-const updateOnFirebase = (channels) => {
-  channels.forEach((channel) => {
-    // =>CHANNEL_IDが該当すればチャンネル登録者数を更新
-    channelsRef.orderByChild('youtube/id').equalTo(channel.id).once('value', (snapshot) => {
-      snapshot.forEach((s) => channelsRef.update({[`/${s.key}/youtube/subscriberCount`]: channel.statistics.subscriberCount}))
+const updateOnFirebase = (channelsResponse) => {
+  console.log('updateOnFirebase')
+  const promiseOnce = channelsResponse.map((channelResponse) => {
+    return channelsRef.orderByChild('youtube/id').equalTo(channelResponse.id).once('value').then((snapshot) => {
+      const channels: ChannelsSnapshot = snapshot.val()
+      const promiseUpdate = Object.keys(channels).map((key, index) => {
+        return channelsRef.update({[`/${key}/youtube/subscriberCount`]: Number(channelResponse.statistics.subscriberCount)})
+      })
+      return Promise.all(promiseUpdate)
     })
+  })
+  return Promise.all(promiseOnce)
+}
+
+const updateScore = () => {
+  console.log('updateScore')
+  return channelsRef.once('value').then((snapshot) => {
+    const channels: ChannelsSnapshot = snapshot.val()
+    const promiseUpdate = Object.keys(channels).map((key, index) => {
+      const _score = channels[key].likeCount + channels[key].youtube.subscriberCount
+      const score = channels[key].twitter ? _score + channels[key].twitter.followersCount : _score
+      return channelsRef.update({[`/${key}/score`]: score})
+    })
+    return Promise.all(promiseUpdate)
   })
 }
 
-// 100件Firebaseからデータを取得
-// =>TW APIで、まとめて投げれるのが100件までだから。
+const updateRank = () => {
+  console.log('updateRank')
+  return channelsRef.orderByChild('score').once('value').then((snapshot) => {
+    const channelKeys = []
+    snapshot.forEach((s) => {
+      channelKeys.push(s.key)
+    })
+    const promiseUpdate = channelKeys.reverse().map((key, index) => {
+      return channelsRef.update({[`/${key}/rank`]: index + 1})
+    })
+    return Promise.all(promiseUpdate)
+  })
+}
+
 export const updateChannels = () => {
-  channelsRef.limitToFirst(LIMIT).once('value')
+  const updateChannelsPromise = channelsRef.limitToFirst(LIMIT).once('value')
     .then((snapshot) => accumulateIds(snapshot))
     .then((ids: {channelIds: string[]}) => toParameter(ids))
     .then((parameters: {channelIds: string}) => getLatestItem(parameters.channelIds))
     .then((response: {channels: Object[]}) => updateOnFirebase(response.channels))
-    .catch((error) => console.log(error))
+    .then(() => updateScore())
+    .then(() => updateRank())
+
+  logFinished(updateChannelsPromise, 'updateChannels')
 }
