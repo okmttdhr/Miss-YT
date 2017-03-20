84203a1462249202ea73be2e23eb28db93b663d9
okmttdhr <10101114fin@gmail.com>
2017-03-20 17:10:15 +0800

    add saga

--- App/Services/resources/index.js
+++ App/Services/resources/index.js
@@ -1,2 +1,10 @@
+export const statusCode = {
+  Ok: 200,
+  InvalidParameters: 400,
+  Forbidden: 403,
+  NotFound: 404,
+  InternalError: 500
+}
+
 export * from './Resource'
 export * from './youtube/'
