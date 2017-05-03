17467d7e46aff0c8af6f4fefc7cb3dcc18e1ed65
okmttdhr <10101114fin@gmail.com>
2017-05-03 10:23:48 +0900

    fix naming collision error after updating react-native-router-flux
    https://github.com/aksonov/react-native-router-flux/issues/1816

--- App/Services/batches/addChannels.js
+++ App/Services/batches/addChannels.js
@@ -9,7 +9,11 @@ import type {TChannelResponse} from '../../types/ChannelResponse';
 // add CHANNEL_IDS before dispatching batch
 const CHANNEL_IDS = `
   UC2rbyOa3Jo7vGSibqKcRjqw, UCI5qMix97T3tVZfxmHObDjA, UC0elp2101KAxbaAMzInGerA, UCB10yM5qyQpNsMNDQ1VHxKg, UCiOm_FmFK4jxB9VRuFC1pag,
-  UCCuizDTLsr-mNm_PEGdChVg
+  UCCuizDTLsr-mNm_PEGdChVg, UCRdQOBEzSpAOtZ_yitD7GpA, UCFTVNLC7ysej-sD5lkLqNGA, UC5xAkS4828lDivq8cKFGSyw, UC__AsSnEuyVgO9TWvZE_ziA,
+  UCfCY70zRsvnnKzQ39mBq0rw, UCr-QcqNToYablI-jU2VPVSw, UCQFvuGmxBmv-c_irVda7mwg, UCLfbmGhvu7xTHXktmMWdfvw, UC69xoWl5-Y3m-oRFTORIbKw,
+  UChcgcbX3zMBwd0wTJyy2JlA, UCAjZx0WhDOjIWsoy0owuK4w, UC0-H-XRuSPbDdPRmAnuzxSQ, UC97ysepeThCEbhrWhYrqxAw, UCjeKoCr7YTNn0bmMO1HdyVw,
+  UCrd9neCNtPkvQukpNTYrP4Q, UCLi8qtd3QLvXCZLyobiodiw, UCtVnAr55ALHAuI7Yyz4GZWg, UCC1BNMUl5dnju1b9oKpkysg, UC36d9BfqBDm4ToLVvPnI1zg,
+  UCsTM1roCxoot1-03EO5zQxg, UCIyMwXronD5pT5cx-G_KSHA
 `;
 
 const createChannel = (channel: TChannelResponse): TChannel => {
