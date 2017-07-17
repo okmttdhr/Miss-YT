2e00e47426c0f988cf952deb79e3bbdb80d745f4
okmttdhr <10101114fin@gmail.com>
2017-07-17 15:22:41 +0900

    remove existing login system

--- App/Redux/Root.js
+++ App/Redux/Root.js
@@ -8,7 +8,6 @@ import { userReducer } from './UserRedux';
 export const createStore = () => {
   const rootReducer = combineReducers({
     temperature: require('./TemperatureRedux').reducer,
-    login: require('./LoginRedux').reducer,
     search: require('./SearchRedux').reducer,
     channels: channelsReducer,
     user: userReducer,
--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -4,12 +4,10 @@ import { takeLatest } from 'redux-saga';
 
 import { channelsTypes, userTypes } from '../Redux/';
 import { StartupTypes } from '../Redux/StartupRedux';
-import { LoginTypes } from '../Redux/LoginRedux';
 
 /* ------------- Sagas ------------- */
 
 import { startup } from './StartupSagas';
-import { login as legacyLogin } from './LoginSagas';
 import { getChannels } from './ChannelsSagas';
 import { login, createUser, updateProfile, sendEmailVerification, reload, sendPasswordResetEmail } from './UserSagas';
 
@@ -18,7 +16,6 @@ import { login, createUser, updateProfile, sendEmailVerification, reload, sendPa
 export default function* root() {
   yield [
     takeLatest(StartupTypes.STARTUP, startup),
-    takeLatest(LoginTypes.LOGIN_REQUEST, legacyLogin),
     takeLatest(channelsTypes.CHANNELS_REQUEST, getChannels),
 
     takeLatest(userTypes.USER_LOGIN, login),
