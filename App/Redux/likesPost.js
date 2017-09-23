478501458df6b716d9c33ab791631ea435c2730a
okmttdhr <10101114fin@gmail.com>
2017-09-23 17:20:42 +0900

    no need for LIKES_POST_SUCCESS/FAILURE

--- App/Redux/likesPost.js
+++ App/Redux/likesPost.js
@@ -1,15 +1,11 @@
 // @flow
-import Immutable from 'seamless-immutable';
-import {findLastKey} from 'lodash';
-import type {TChannelStoreWithKey, TChannelStore} from '../types/';
+import type {TChannelStore} from '../types/';
 
 /* ------------- Types and Action Creators ------------- */
 
 export const likesPostActions = (type: string) => {
   return {
     [`${type}LikesPostRequest`]: ['channel'],
-    likesPostSuccess: ['item'],
-    likesPostFailure: ['channelId', 'errorMessage'],
     [`${type}LikesPostIncrease`]: ['item'],
   };
 };
@@ -20,19 +16,6 @@ export const likesPostReducer = (TYPE: string) => {
   return {
     [`${TYPE}_LIKES_POST_REQUEST`]: (state: Object, { channel }: {channel: TChannelStore}) => {
       const update = {[channel.id]: {isFetching: true, errorMessage: ''}};
-      return state.merge({
-        items: update,
-      }, {deep: true});
-    },
-    LIKES_POST_SUCCESS: (state: Object, { item }: {item: TChannelStoreWithKey}) => {
-      const key = item[findLastKey(item)].id;
-      const update = {[key]: {isFetching: false, errorMessage: ''}};
-      return state.merge({
-        items: Immutable(item).merge(update, {deep: true}),
-      }, {deep: true});
-    },
-    LIKES_POST_FAILURE: (state: Object, { channelId, errorMessage }: Object) => {
-      const update = {[channelId]: {isFetching: false, errorMessage}};
       return state.merge({
         items: update,
       }, {deep: true});
