2f891cab8c4e93c90adb6b64d38940df9f085f7a
okmttdhr <10101114fin@gmail.com>
2017-11-30 20:40:59 +0900

    open link

--- App/Components/ChannelDetail/Videos/Panel/index.js
+++ App/Components/ChannelDetail/Videos/Panel/index.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking, TouchableOpacity } from 'react-native';
 
 import styles from './style';
 import type {TVideo} from '../../../../types/';
@@ -10,8 +10,24 @@ type TPanel = {
 }
 
 export const Panel = ({video}: TPanel) => (
-  <View style={styles.container}>
-    <Image style={styles.image} source={{uri: video.thumbnail}} />
-    <Text style={styles.title} numberOfLines={4}>{video.title}</Text>
-  </View>
+  <TouchableOpacity
+    activeOpacity={0.8}
+    onPress={() => {
+      const url = `https://www.youtube.com/watch?v=${video.videoId}`;
+      Linking.canOpenURL(url).then((supported) => {
+        if (!supported) {
+          return;
+        }
+        Linking.openURL(url);
+      }).catch();
+    }}
+  >
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{uri: video.thumbnail}}
+      />
+      <Text style={styles.title} numberOfLines={4}>{video.title}</Text>
+    </View>
+  </TouchableOpacity>
 );
